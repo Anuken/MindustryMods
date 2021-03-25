426a101791f23683db6189ff6855b9aa2f39089f
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-25 20:05:34 +0800

    Update ModUpdater.java

--- src/modupdater/ModUpdater.java
+++ src/modupdater/ModUpdater.java
@@ -23,7 +23,7 @@ public class ModUpdater{
     static final int perPage = 100;
     static final int maxLength = 55;
     static final ObjectSet<String> javaLangs = ObjectSet.with("Java", "Kotlin", "Groovy"); //obviously not a comprehensive list
-    static final ObjectSet<String> blacklist = ObjectSet.with("Snow-of-Spirit-Fox-Mori/old-mod", "TheSaus/Cumdustry", "Anuken/ExampleMod", "Anuken/ExampleJavaMod", "Anuken/ExampleKotlinMod", "Mesokrix/Vanilla-Upgraded", "o7-Fire/Mindustry-Ozone");
+    static final ObjectSet<String> blacklist = ObjectSet.with("Snow-of-Spirit-Fox-Mori/old-mod", "TheSaus/Cumdustry", "Anuken/ExampleMod", "Anuken/ExampleJavaMod", "Anuken/ExampleKotlinMod", "Mesokrix/Vanilla-Upgraded", "o7-Fire/Minudstry-Ozone");
     static final int iconSize = 64;
 
     public static void main(String[] args){
