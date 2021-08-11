a546c23da5ad6ee9ab6ead3ee2a3831e9a722778
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-08-11 13:34:28 +0200

    Blacklist EasyPlaySu/BBM
    They are stealing sprites from every mod you can imagine, there are about 30-50 stolen sprites, from mods like Exotic, Betamindy, HAI, and some others

--- src/modupdater/ModUpdater.java
+++ src/modupdater/ModUpdater.java
@@ -21,7 +21,7 @@ public class ModUpdater{
     static final int perPage = 100;
     static final int maxLength = 55;
     static final ObjectSet<String> javaLangs = ObjectSet.with("Java", "Kotlin", "Groovy", "Scala"); //obviously not a comprehensive list
-    static final ObjectSet<String> blacklist = ObjectSet.with("Snow-of-Spirit-Fox-Mori/old-mod", "TheSaus/Cumdustry", "Anuken/ExampleMod", "Anuken/ExampleJavaMod", "Anuken/ExampleKotlinMod", "Mesokrix/Vanilla-Upgraded", "o7-Fire/Mindustry-Ozone");
+    static final ObjectSet<String> blacklist = ObjectSet.with("Snow-of-Spirit-Fox-Mori/old-mod", "TheSaus/Cumdustry", "Anuken/ExampleMod", "Anuken/ExampleJavaMod", "Anuken/ExampleKotlinMod", "Mesokrix/Vanilla-Upgraded", "o7-Fire/Mindustry-Ozone", "EasyPlaySu/BBM");
     static final Seq<String> nameBlacklist = Seq.with("o7", "Iron-Miner");
     static final int iconSize = 64;
 
