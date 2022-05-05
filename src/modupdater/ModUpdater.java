97ab48968aae9e66ec38715061b43667c903a81c
Anuken <arnukren@gmail.com>
2022-05-05 00:29:09 -0400

    even more threads

--- src/modupdater/ModUpdater.java
+++ src/modupdater/ModUpdater.java
@@ -103,7 +103,7 @@ public class ModUpdater{
             Log.info("&lcTotal mods found: @\n", names.size);
 
             //awful.
-            ExecutorService exec = Threads.executor(50);
+            ExecutorService exec = Threads.executor(60);
 
             AtomicInteger index = new AtomicInteger();
             for(String name : names){
